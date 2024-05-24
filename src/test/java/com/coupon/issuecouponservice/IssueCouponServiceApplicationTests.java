6c901bf1952fe0df92e3eb06ef10476d15c6b033
ChuJH <09strawberry@naver.com>
2024-05-24 17:06:09 +0900

    [refactor] 7 - 다시 복구

--- src/test/java/com/coupon/issuecouponservice/IssueCouponServiceApplicationTests.java
+++ src/test/java/com/coupon/issuecouponservice/IssueCouponServiceApplicationTests.java
@@ -1,12 +1,13 @@
 package com.coupon.issuecouponservice;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = IssueCouponServiceApplicationTests.class)
 class IssueCouponServiceApplicationTests {
 
-/*	@Test
+	@Test
 	void contextLoads() {
-	}*/
+	}
 
 }
