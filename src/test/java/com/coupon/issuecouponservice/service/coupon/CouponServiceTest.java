ac0cb84810b2204ad75b66b258c4107644f68958
ChuJH <09strawberry@naver.com>
2024-05-24 21:59:01 +0900

    [refactor] 성공 이후 - build.gradle tasks.named("test") 주석 + test 주석 + redis import 취소

--- src/test/java/com/coupon/issuecouponservice/service/coupon/CouponServiceTest.java
+++ src/test/java/com/coupon/issuecouponservice/service/coupon/CouponServiceTest.java
@@ -4,7 +4,6 @@ import com.coupon.issuecouponservice.domain.coupon.Coupon;
 import com.coupon.issuecouponservice.domain.user.Role;
 import com.coupon.issuecouponservice.domain.user.User;
 import com.coupon.issuecouponservice.dto.request.coupon.CouponIssueParam;
-import com.coupon.issuecouponservice.facade.RedisLockStockFacade;
 import com.coupon.issuecouponservice.repository.coupon.CouponRepository;
 import com.coupon.issuecouponservice.repository.coupon.UserCouponRepository;
 import com.coupon.issuecouponservice.repository.user.UserRepository;
@@ -16,18 +15,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @TestPropertySource(locations = "/application-test.properties")
@@ -37,8 +30,8 @@ class CouponServiceTest {
     @Autowired
     private CouponService couponService;
 
-    @Autowired
-    private RedisLockStockFacade redisLockStockFacade;
+/*    @Autowired
+    private RedisLockStockFacade redisLockStockFacade;*/
 
     @Autowired
     private UserCouponRepository userCouponRepository;
@@ -109,7 +102,7 @@ class CouponServiceTest {
         AssertionsForClassTypes.assertThat(count).isEqualTo(1L);
     }
 
-    @Test
+/*    @Test
     @DisplayName("쿠폰 여러 명 발급 - 레디슨")
     void issueCoupon_redisson() throws InterruptedException {
         int threadCount = 1000;
@@ -142,5 +135,5 @@ class CouponServiceTest {
         int count = userCouponRepository.countByCouponId(couponIssueParam.getCouponId());
 
         assertThat(count).isEqualTo(100);
-    }
+    }*/
 }
