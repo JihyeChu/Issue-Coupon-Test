1b8ec5918714658c1f148c91d5574743b2e65bc0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-24 22:44:28 +0900

    redisson 추가 전으로 복귀

--- src/test/java/com/coupon/issuecouponservice/service/coupon/CouponServiceTest.java
+++ src/test/java/com/coupon/issuecouponservice/service/coupon/CouponServiceTest.java
@@ -4,7 +4,6 @@ import com.coupon.issuecouponservice.domain.coupon.Coupon;
 import com.coupon.issuecouponservice.domain.user.Role;
 import com.coupon.issuecouponservice.domain.user.User;
 import com.coupon.issuecouponservice.dto.request.coupon.CouponIssueParam;
-import com.coupon.issuecouponservice.facade.RedisLockStockFacade;
 import com.coupon.issuecouponservice.repository.coupon.CouponRepository;
 import com.coupon.issuecouponservice.repository.coupon.UserCouponRepository;
 import com.coupon.issuecouponservice.repository.user.UserRepository;
@@ -16,18 +15,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @TestPropertySource(locations = "/application-test.properties")
@@ -37,8 +30,8 @@ class CouponServiceTest {
     @Autowired
     private CouponService couponService;
 
-    @Autowired
-    private RedisLockStockFacade redisLockStockFacade;
+//    @Autowired
+//    private RedisLockStockFacade redisLockStockFacade;
 
     @Autowired
     private UserCouponRepository userCouponRepository;
@@ -105,38 +98,38 @@ class CouponServiceTest {
         AssertionsForClassTypes.assertThat(count).isEqualTo(1L);
     }
 
-    @Test
-    @DisplayName("쿠폰 여러 명 발급 - 레디슨")
-    void issueCoupon_redisson() throws InterruptedException {
-        int threadCount = 1000;
-        ExecutorService executorService = Executors.newFixedThreadPool(32);
-        CountDownLatch latch = new CountDownLatch(threadCount);
-
-        for (int i = 0; i < threadCount; i++) {
-            final int threadNumber = i + 1;
-            User user = users.get(i);
-            executorService.submit(() -> {
-                try {
-                    redisLockStockFacade.issueCoupon(couponIssueParam, user);
-                    System.out.println("Thread " + threadNumber + " - 성공");
-
-                } catch (PessimisticLockingFailureException e) {
-                    System.out.println("Thread " + threadNumber + " - 락 충돌 감지");
-
-                } catch (Exception e) {
-                    System.out.println("Thread " + threadNumber + " - " + e.getMessage());
-
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await();
-        executorService.shutdown();
-
-        int count = userCouponRepository.countByCouponId(couponIssueParam.getCouponId());
-
-        assertThat(count).isEqualTo(100);
-    }
+//    @Test
+//    @DisplayName("쿠폰 여러 명 발급 - 레디슨")
+//    void issueCoupon_redisson() throws InterruptedException {
+//        int threadCount = 1000;
+//        ExecutorService executorService = Executors.newFixedThreadPool(32);
+//        CountDownLatch latch = new CountDownLatch(threadCount);
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            final int threadNumber = i + 1;
+//            User user = users.get(i);
+//            executorService.submit(() -> {
+//                try {
+//                    redisLockStockFacade.issueCoupon(couponIssueParam, user);
+//                    System.out.println("Thread " + threadNumber + " - 성공");
+//
+//                } catch (PessimisticLockingFailureException e) {
+//                    System.out.println("Thread " + threadNumber + " - 락 충돌 감지");
+//
+//                } catch (Exception e) {
+//                    System.out.println("Thread " + threadNumber + " - " + e.getMessage());
+//
+//                } finally {
+//                    latch.countDown();
+//                }
+//            });
+//        }
+//
+//        latch.await();
+//        executorService.shutdown();
+//
+//        int count = userCouponRepository.countByCouponId(couponIssueParam.getCouponId());
+//
+//        assertThat(count).isEqualTo(100);
+//    }
 }
