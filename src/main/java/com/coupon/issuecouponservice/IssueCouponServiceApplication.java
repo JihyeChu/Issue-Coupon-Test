a7353bf5ee58152ca4149b8faa05e7ae7973ac9b
ChuJH <09strawberry@naver.com>
2024-05-24 12:41:56 +0900

    [refactor] 환경변수 설정

--- src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
+++ src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 import java.util.TimeZone;
 
 @EnableJpaAuditing
@@ -21,16 +18,6 @@ public class IssueCouponServiceApplication {
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-		Properties props = new Properties();
-		try (FileInputStream fis = new FileInputStream("src/test/resources/application-test.properties")) {
-			props.load(fis);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		System.setProperty("spring.datasource.url", props.getProperty("DB_URL"));
-		System.setProperty("spring.datasource.username", props.getProperty("DB_USER_NAME"));
-		System.setProperty("spring.datasource.password", props.getProperty("DB_PASSWORD"));
 	}
 
 }
